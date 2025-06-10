362a613a39d9bafed9bf946358d437e96d3d7609
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-06-10 14:26:03 +0530

    Rick & Morty

---
+++ Assesment/U3 PAI2/Rick & Morty APP/js/script.js
@@ -0,0 +1,51 @@
+const api = "https://rickandmortyapi.com/api/character";
+let currentPage = api;
+
+// fetch
+async function fetchData(newUrl) {
+  let res = await fetch(newUrl);
+  let data = await res.json();
+
+  dispData(data.results);
+  // paginationData(data.info);
+}
+
+// display
+function dispData(data) {
+  let container = document.getElementById("charContainer");
+  container.innerHTML = "";
+
+  data.forEach((character) => {
+    let card = document.createElement("div");
+    card.className = "card";
+
+    card.innerHTML = `
+      <img src="${character.image}" alt="${character.image}">
+      <h4>${character.name}</h4>
+      <p>${character.species} - ${character.status}</p>
+      <p>Location: ${character.location.name}</p>
+    `;
+    card.addEventListener("click", () => openCharacter(character));
+    container.appendChild(card);
+  });
+}
+
+// pagination
+let pNO = 1;
+document.getElementById('next-btn').addEventListener('click',()=>{
+  cPage = `https://rickandmortyapi.com/api/character?page=${++pNO}`
+  fetchData(cPage)
+})
+document.getElementById('prev-btn').addEventListener('click',()=>{
+  cPage = `https://rickandmortyapi.com/api/character?page=${--pNO}`
+  if(pNO==0) pNO=1
+  fetchData(cPage)
+})
+
+
+fetchData(currentPage);
+
+// newPage
+function openCharacter(character) {
+  console.log("hello");
+}
