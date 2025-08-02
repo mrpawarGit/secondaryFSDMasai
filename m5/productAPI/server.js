62e95d94d63977668f34be4d7f0ea2903d230d8d
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-08-02 20:26:24 +0530

    Product API - env

--- m5/productAPI/server.js
+++ m5/productAPI/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const connectDB = require("./db");
 
 const app = express();
+app.use(express.json());
 
-app.listen(3000, () => {
+app.listen(3000, async () => {
+  await connectDB();
   console.log("Server Started at http://localhost:3000/");
 });
