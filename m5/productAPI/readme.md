3f898c870c0140cf3233e574f27f9c728001d1ee
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-08-02 21:55:06 +0530

    Product API - Update and delete Product

---
+++ m5/productAPI/readme.md
@@ -0,0 +1,9 @@
+# E-Commerce Product API
+
+## done with
+
+- 0. Schemas & Basic setup
+- 1. Get All Products
+- 2. Add New Product
+- 3. Delete Product by ID
+- 4. Update Product by ID
--- m5/productAPI/routes/productRoutes.js
+++ m5/productAPI/routes/productRoutes.js
@@ -2,11 +2,15 @@ const express = require("express");
 const {
   createProduct,
   getAllProducts,
+  deleteProduct,
+  updateProduct,
 } = require("../controllers/product.controller");
 
 const router = express.Router();
 
 router.get("/", getAllProducts);
 router.post("/", createProduct);
+router.delete("/:id", deleteProduct);
+router.patch("/:id", updateProduct);
 
 module.exports = router;
