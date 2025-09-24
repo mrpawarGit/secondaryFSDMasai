486e3d724189f69f772dee390e811d21799c0fad
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-09-24 12:46:23 +0530

    basic server.js setup done with .env file

---
+++ AU5P2/Account Recovery API/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// parser
+app.use(express.json());
+
+// test route
+app.get("/test", (req, res) => {
+  res.status(200).json({ msg: "Test Route" });
+});
+
+// invalid route handle
+app.use((req, res) => {
+  res.status(400).json({ msg: "Not Found" });
+});
+
+app.listen(PORT, () => {
+  console.log("Server Started at Port", PORT);
+});
