cd31630e9f3179558a1ac444cf707465e6eaa199
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-10-13 11:52:38 +0530

    DB Connected

--- Coding Contest 2 FullStack/LMS/backend/server.js
+++ Coding Contest 2 FullStack/LMS/backend/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const connectDB = require("./config/db");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 app.get("/test", (req, res) => {
   res.json({ msg: "Test Route" });
